d471491a770be2285729a14e5c7f793c91f4ce18
emerix <rafalax@gmail.com>
2014-03-10 18:23:38 +0700

    selection checkbox on headers

--- FlickrUploader/src/com/rafali/flickruploader/FlickrUploaderActivity.java
+++ FlickrUploader/src/com/rafali/flickruploader/FlickrUploaderActivity.java
@@ -6,11 +6,14 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -52,8 +55,6 @@ import android.widget.Toast;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.google.common.base.Functions;
 import com.google.common.base.Joiner;
-import com.google.common.base.Predicates;
-import com.google.common.collect.Collections2;
 import com.google.common.collect.Ordering;
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.Background;
@@ -196,10 +197,16 @@ public class FlickrUploaderActivity extends Activity {
 		String title;
 		int count = 1;
 		boolean collapsed = false;
+		boolean selected = false;
 
 		Header(String id, String title) {
 			this.id = id;
 			this.title = title;
+		}
+
+		@Override
+		public String toString() {
+			return id + ":" + count + ":" + title + ":collapsed=" + collapsed + ":selected=" + selected;
 		}
 	}
 
@@ -287,7 +294,7 @@ public class FlickrUploaderActivity extends Activity {
 				mainTabView = (com.tonicartos.widget.stickygridheaders.StickyGridHeadersGridView) View.inflate(FlickrUploaderActivity.this, R.layout.stickygrid, null);
 				final PhotoAdapter photoAdapter = new PhotoAdapter();
 				mainTabView.setAdapter(photoAdapter);
-				// mainTabView.setOnItemClickListener(onItemClickListener);
+				mainTabView.setOnItemClickListener(onItemClickListener);
 				mainTabView.setOnHeaderClickListener(new StickyGridHeadersGridView.OnHeaderClickListener() {
 					@Override
 					public void onHeaderClick(AdapterView<?> arg0, View arg1, long headerPosition) {
@@ -332,38 +339,43 @@ public class FlickrUploaderActivity extends Activity {
 	OnItemClickListener onItemClickListener = new OnItemClickListener() {
 		@Override
 		public void onItemClick(AdapterView<?> arg0, View convertView, int arg2, long arg3) {
-			// FlickrApi.isUploaded(isFolderTab() ?
-			// foldersMap.get(convertView.getTag()) : convertView.getTag());
-			if (getImageSelected(SelectionType.all).contains(convertView.getTag())) {
+			Media media = (Media) convertView.getTag();
+			Header header = headerMap.get(media);
+			if (selectedMedia.contains(media)) {
 				convertView.findViewById(R.id.check_image).setVisibility(View.GONE);
-				getImageSelected(SelectionType.all).remove(convertView.getTag());
+				selectedMedia.remove(media);
+				if (header.selected) {
+					header.selected = false;
+				}
 			} else {
 				convertView.findViewById(R.id.check_image).setVisibility(View.VISIBLE);
-				getImageSelected(SelectionType.all).add((Media) convertView.getTag());
+				selectedMedia.add(media);
+				if (!header.selected) {
+					List<Media> headerMedias = new ArrayList<Media>();
+					Iterator<Entry<Media, Header>> it = headerMap.entrySet().iterator();
+					while (it.hasNext()) {
+						Map.Entry<Media, Header> entry = it.next();
+						if (entry.getValue() == header) {
+							headerMedias.add(entry.getKey());
+						}
+					}
+					if (selectedMedia.containsAll(headerMedias)) {
+						header.selected = true;
+					}
+				}
 			}
-			updateCount();
+			renderSelection();
 		}
 	};
 
 	void updateCount() {
 		int size = 0;
-		// if (isFolderTab()) {
-		// for (Media image : getImageSelected(SelectionType.all)) {
-		// Folder folder = foldersMap.get(image);
-		// if (folder != null)
-		// size += folder.size;
-		// }
-		// } else {
-		size = getImageSelected(SelectionType.all).size();
-		// }
+		size = selectedMedia.size();
 		if (size > 0) {
 			if (mMode == null)
 				mMode = startActionMode(mCallback);
 			if (mMode != null)
 				mMode.setTitle(size + "");
-			// if (!isFolderTab()) {
-			// setShareIntent();
-			// }
 		} else if (size == 0 && mMode != null) {
 			mMode.finish();
 		}
@@ -374,9 +386,9 @@ public class FlickrUploaderActivity extends Activity {
 
 	@UiThread(delay = 200)
 	void setShareIntent() {
-		if (!getImageSelected(SelectionType.all).isEmpty() && shareItem != null) {
+		if (!selectedMedia.isEmpty() && shareItem != null) {
 			Map<String, String> shortUrls = new LinkedHashMap<String, String>();
-			for (Media image : getImageSelected(SelectionType.all)) {
+			for (Media image : selectedMedia) {
 				String photoId = FlickrApi.getPhotoId(image);
 				if (photoId != null) {
 					shortUrls.put(photoId, FlickrApi.getShortUrl(photoId));
@@ -411,7 +423,10 @@ public class FlickrUploaderActivity extends Activity {
 		@Override
 		public void onDestroyActionMode(ActionMode mode) {
 			mMode = null;
-			getImageSelected(SelectionType.all).clear();
+			selectedMedia.clear();
+			for (Header header : headers) {
+				header.selected = false;
+			}
 			renderSelection();
 		}
 
@@ -457,24 +472,9 @@ public class FlickrUploaderActivity extends Activity {
 			switch (item.getItemId()) {
 			case R.id.menu_item_select_all: {
 				EasyTracker.getTracker().sendEvent("ui", "click", "menu_item_select_all", 0L);
-				if (getImageSelected(SelectionType.auto).isEmpty()) {
-					Collection<Media> allImages;
-					// if (isFolderTab()) {
-					// allImages = foldersMap.keySet();
-					// } else if (isVideoTab()) {
-					// allImages = videos;
-					// } else {
-					allImages = photos;
-					// }
-					getImageSelected(SelectionType.auto).addAll(allImages);
-					getImageSelected(SelectionType.auto).removeAll(getImageSelected(SelectionType.all));
-					getImageSelected(SelectionType.all).clear();
-					getImageSelected(SelectionType.all).addAll(allImages);
-				} else {
-					getImageSelected(SelectionType.all).removeAll(getImageSelected(SelectionType.auto));
-					getImageSelected(SelectionType.auto).clear();
-				}
-				updateCount();
+				selectedMedia.clear();
+				selectedMedia.addAll(photos);
+
 				renderSelection();
 			}
 				break;
@@ -483,11 +483,11 @@ public class FlickrUploaderActivity extends Activity {
 				Collection<Media> selectedImages;
 				// if (isFolderTab()) {
 				// selectedImages = new ArrayList<Media>();
-				// for (Media image : getImageSelected(SelectionType.all)) {
+				// for (Media image : selectedMedia) {
 				// selectedImages.addAll(foldersMap.get(image).images);
 				// }
 				// } else {
-				selectedImages = getImageSelected(SelectionType.all);
+				selectedImages = selectedMedia;
 				// }
 				PRIVACY privacy = null;
 				for (Media image : selectedImages) {
@@ -505,7 +505,7 @@ public class FlickrUploaderActivity extends Activity {
 					public void onResult(PRIVACY result) {
 						if (result != null) {
 							List<String> photoIds = new ArrayList<String>();
-							for (Media image : getImageSelected(SelectionType.all)) {
+							for (Media image : selectedMedia) {
 								String photoId = FlickrApi.getPhotoId(image);
 								if (photoId != null && FlickrApi.getPrivacy(photoId) != result) {
 									photoIds.add(photoId);
@@ -521,7 +521,7 @@ public class FlickrUploaderActivity extends Activity {
 				break;
 			case R.id.menu_item_upload: {
 				EasyTracker.getTracker().sendEvent("ui", "click", "menu_item_upload", 0L);
-				final List<Media> selection = new ArrayList<Media>(getImageSelected(SelectionType.all));
+				final List<Media> selection = new ArrayList<Media>(selectedMedia);
 				if (FlickrApi.isAuthentified()) {
 					// foldersMap.clear();
 					BackgroundExecutor.execute(new Runnable() {
@@ -538,7 +538,7 @@ public class FlickrUploaderActivity extends Activity {
 				break;
 			case R.id.menu_item_dequeue: {
 				EasyTracker.getTracker().sendEvent("ui", "click", "menu_item_dequeue", 0L);
-				final List<Media> selection = new ArrayList<Media>(getImageSelected(SelectionType.all));
+				final List<Media> selection = new ArrayList<Media>(selectedMedia);
 				BackgroundExecutor.execute(new Runnable() {
 					@Override
 					public void run() {
@@ -799,29 +799,43 @@ public class FlickrUploaderActivity extends Activity {
 
 	@UiThread
 	void renderSelection() {
-		ViewGroup grid = (ViewGroup) mainTabView;
-		int childCount = grid.getChildCount();
+		int childCount = mainTabView.getChildCount();
 		for (int i = 0; i < childCount; i++) {
-			View view = grid.getChildAt(i);
-			view.findViewById(R.id.check_image).setVisibility(getImageSelected(SelectionType.all).contains(view.getTag()) ? View.VISIBLE : View.GONE);
+			View view = mainTabView.getChildAt(i);
+			View check_image = view.findViewById(R.id.check_image);
+			if (check_image == null) {
+				// if (view.getTag() instanceof View) {
+				// View headerView = (View) view.getTag();
+				// renderHeaderSelection(headerView);
+				// }
+			} else {
+				check_image.setVisibility(selectedMedia.contains(view.getTag()) ? View.VISIBLE : View.GONE);
+			}
+		}
+		for (View headerView : attachedHeaderViews) {
+			renderHeaderSelection(headerView);
+		}
+		// for (int i = 0; i < headers.size(); i++) {
+		// renderHeaderSelection(mainTabView.getHeaderAt(i));
+		// }
+		// renderHeaderSelection(mainTabView.getStickiedHeader());
+		// PhotoAdapter adapter = (PhotoAdapter) mainTabView.getAdapter();
+		// adapter.notifyDataSetChanged();
+		mainTabView.requestLayout();
+		updateCount();
+	}
+
+	void renderHeaderSelection(View headerView) {
+		if (headerView != null && headerView.getTag() instanceof Header) {
+			Header header = (Header) headerView.getTag();
+			// LOG.debug("rendering : " + header + " on " + headerView);
+			CheckBox checkbox = (CheckBox) headerView.getTag(R.id.checkbox);
+			checkbox.setChecked(header.selected);
+			// checkbox.setText("" + header.selected);
 		}
 	}
 
-	Map<String, List<Media>> imagesSelectedMap = new HashMap<String, List<Media>>();
-
-	private List<Media> getImageSelected(SelectionType type) {
-		String key = mainTabView.getClass().getSimpleName() + "_" + mainTabView.getTag() + "_" + type;
-		List<Media> imagesSelected = imagesSelectedMap.get(key);
-		if (imagesSelected == null) {
-			imagesSelected = new ArrayList<Media>();
-			imagesSelectedMap.put(key, imagesSelected);
-		}
-		return imagesSelected;
-	}
-
-	enum SelectionType {
-		all, auto
-	}
+	Set<Media> selectedMedia = new HashSet<Media>();
 
 	List<Media> medias;
 	List<Media> photos;
@@ -870,20 +884,57 @@ public class FlickrUploaderActivity extends Activity {
 
 		@Override
 		public View getHeaderView(int position, View convertView, ViewGroup arg2) {
+			final CheckBox checkBox;
+			final TextView textView;
 			if (convertView == null) {
 				convertView = View.inflate(activity, R.layout.grid_header, null);
+				checkBox = (CheckBox) convertView.findViewById(R.id.checkbox);
+				checkBox.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						Header header = (Header) v.getTag();
+						header.selected = checkBox.isChecked();
+						Iterator<Entry<Media, Header>> it = headerMap.entrySet().iterator();
+						while (it.hasNext()) {
+							Map.Entry<Media, Header> entry = it.next();
+							if (entry.getValue() == header) {
+								if (header.selected) {
+									selectedMedia.add(entry.getKey());
+								} else {
+									selectedMedia.remove(entry.getKey());
+								}
+							}
+						}
+						renderSelection();
+					}
+				});
+				convertView.setTag(R.id.checkbox, checkBox);
+				textView = (TextView) convertView.findViewById(R.id.title);
+				convertView.setTag(R.id.title, textView);
+				convertView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+					@Override
+					public void onViewDetachedFromWindow(View v) {
+						attachedHeaderViews.remove(v);
+					}
+
+					@Override
+					public void onViewAttachedToWindow(View v) {
+						attachedHeaderViews.add(v);
+						renderHeaderSelection(v);
+					}
+				});
+			} else {
+				checkBox = (CheckBox) convertView.getTag(R.id.checkbox);
+				textView = (TextView) convertView.getTag(R.id.title);
 			}
 			Header header = headers.get(position);
-			TextView textView = (TextView) convertView.findViewById(R.id.title);
-			textView.setText("pos : " + position + " : " + header.title);
-			CheckBox checkBox = (CheckBox) convertView.findViewById(R.id.checkbox);
-			checkBox.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					LOG.debug("onClick " + v);
-				}
-			});
-			checkBox.setText("" + position);
+			convertView.setTag(header);
+			checkBox.setTag(header);
+
+			textView.setText((header.collapsed ? "> " : "v ") + " " + position + " : " + header.title);
+			renderHeaderSelection(convertView);
+
+			// LOG.debug(header + " : convertView = " + convertView);
 
 			return convertView;
 		}
@@ -901,6 +952,8 @@ public class FlickrUploaderActivity extends Activity {
 			return headers.size();
 		}
 	}
+
+	Set<View> attachedHeaderViews = new HashSet<View>();
 
 	ExecutorService executorService = Executors.newSingleThreadExecutor();
 
@@ -957,7 +1010,7 @@ public class FlickrUploaderActivity extends Activity {
 								bitmapDrawable = null;
 							}
 						}
-						final boolean isChecked = getImageSelected(SelectionType.all).contains(image);
+						final boolean isChecked = selectedMedia.contains(image);
 
 						runOnUiThread(new Runnable() {
 							@Override
